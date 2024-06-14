58c04ec0e020fdf20b3f9d7616e71e9f1feb57b3
jamesjoy-2k24 <jamesjoy2k24@gmail.com>
2024-06-14 21:33:56 +0530

    updated set up environment

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,17 +3,21 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-blue-600 text-white p-4 shadow-lg shadow-black w-[98%] mx-auto rounded-md mt-2">
-      <nav className="container mx-auto flex justify-between items-center">
+    <header className="bg-white shadow-md py-4">
+      <nav className="container mx-auto flex items-center justify-between">
         <Link href="/" legacyBehavior>
-          <a className="text-xl font-bold">Pro-Pulse</a>
+          <a className="font-bold text-red-700 text-2xl">Pro-Pulse</a>
         </Link>
-        <div>
+        <div className="flex items-center gap-5">
           <Link href="/login" legacyBehavior>
-            <a className="mr-4">Login</a>
+            <a className="text-white font-bold shadow-md shadow-black bg-blue-500 py-1 px-8 rounded-md">
+              Login
+            </a>
           </Link>
           <Link href="/register" legacyBehavior>
-            <a>Register</a>
+            <a className="text-red-700 font-bold shadow-md shadow-black bg-transparent py-1 px-6 rounded-md hover:bg-blue-500 hover:text-white duration-200">
+              Register
+            </a>
           </Link>
         </div>
       </nav>
