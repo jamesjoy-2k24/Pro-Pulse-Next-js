7d44d8a255b3d6893f580f40f4e89615b6d336f6
jamesjoy-2k24 <jamesjoy2k24@gmail.com>
2024-06-14 21:20:08 +0530

    Basic environment setup finished

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,49 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
-
-const headerVariants = {
-  hidden: { opacity: 0, y: -20 },
-  visible: { opacity: 1, y: 0 },
-};
-
-const linkVariants = {
-  hover: { scale: 1.1, textShadow: '0px 0px 8px rgb(255, 255, 255)' },
-};
 
 const Header: React.FC = () => {
   return (
-    <motion.header
-      className="bg-blue-600 text-white p-4 shadow-lg"
-      initial="hidden"
-      animate="visible"
-      variants={headerVariants}
-      transition={{ duration: 0.5 }}
-    >
+    <header className="bg-blue-600 text-white p-4 shadow-lg shadow-black w-[98%] mx-auto rounded-md mt-2">
       <nav className="container mx-auto flex justify-between items-center">
         <Link href="/" legacyBehavior>
-          <a className="text-xl font-bold">
-            <motion.div whileHover={{ scale: 1.1 }}>Pro-Pulse</motion.div>
-          </a>
+          <a className="text-xl font-bold">Pro-Pulse</a>
         </Link>
-        <div className="flex space-x-4">
+        <div>
           <Link href="/login" legacyBehavior>
-            <a>
-              <motion.div variants={linkVariants} whileHover="hover">
-                Login
-              </motion.div>
-            </a>
+            <a className="mr-4">Login</a>
           </Link>
           <Link href="/register" legacyBehavior>
-            <a>
-              <motion.div variants={linkVariants} whileHover="hover">
-                Register
-              </motion.div>
-            </a>
+            <a>Register</a>
           </Link>
         </div>
       </nav>
-    </motion.header>
+    </header>
   );
 };
 
